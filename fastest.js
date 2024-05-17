66616ce2a29964888669761062cfa513353812a3
AlekseiGreen <120323580+AlekseiGreen@users.noreply.github.com>
2024-05-17 12:19:52 +0300

    Delete fastest.js

--- fastest.js
+++
@@ -1,8 +0,0 @@
-key = AIzaSyC2XD5L6WHFV8Mtjhs0yU0aDr7B97i0tk0
-
-https://www.googleapis.com/books/v1/volumes?q="subject:Business"&key=AIzaSyC2XD5L6WHFV8Mtjhs0yU0aDr7B97i0tk0&printType=books&startIndex=0&maxResults=6&langRestrict=en
-https://www.googleapis.com/books/v1/volumes?q=flowers+inauthor:keyes&key=AIzaSyC2XD5L6WHFV8Mtjhs0yU0aDr7B97i0tk0
-
-GET https://www.googleapis.com/books/v1/volumes/zyTCAlFPjgYC?key=AIzaSyC2XD5L6WHFV8Mtjhs0yU0aDr7B97i0tk0 //определенный id
-
-http://play.google.com/books/reader?id=9GwrmHRl490C&hl=&source=AIzaSyC2XD5L6WHFV8Mtjhs0yU0aDr7B97i0tk0
